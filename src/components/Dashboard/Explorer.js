0960ce809607b1800d2637f72239265e123c160b
Agustin Espana <agustinespania@gmail.com>
2022-03-10 13:08:54 -0300

    fixes

--- src/components/Dashboard/Explorer.js
+++ src/components/Dashboard/Explorer.js
@@ -2,16 +2,11 @@ import styles from "../../styles/Subjects.module.scss";
 
 const Explorer = (props) => {
 
-	const mappedAreas = (mappedField) => {
-		console.log(mappedField.areas);
-		mappedField.areas.map(area => <li key={area.id}>{area.title}</li>)
-	}
-
 	const mappedFields = props.fields.map(field => 
 		<div key={field.id}>
 			<label>{field.title}</label>
 			<ul>
-			{mappedAreas(field)}
+			{field.areas.map(area => <li key={area.id}>{area.title}</li>)}
 			</ul>
 		</div>
 	);
