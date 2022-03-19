b9107f8e04b6e388b76715186f86951a3a7edbe6
Agustin Espana <agustinespania@gmail.com>
2022-03-19 14:44:31 -0300

    fixed pie colors

--- src/components/Dashboard/Stats.js
+++ src/components/Dashboard/Stats.js
@@ -3,8 +3,16 @@ import styles from "../../styles/Stats.module.scss";
 
 const Stats = (props) => {
 
-	const statsByField = [];
+	const sortByHighestValue = (a, b) => {
+		const fa = a.value;
+		const fb = b.value;
+		
+		if (fa < fb) return -1;
+		else if (fa > fb) return 1;
+		else return 0;
+	}
 
+	const statsByField = [];
 	props.fields.forEach(fieldObject => {
 		const values = props.subjects.filter(subject => (subject.field === fieldObject.field && subject.status !== "Wish")).length;
 		values > 0 &&
@@ -12,9 +20,10 @@ const Stats = (props) => {
 				name: fieldObject.field,
 				value: values
 			})
-	});
+	})
+	const sortedStatsByField = statsByField.sort(sortByHighestValue);
 
-	const COLORS = ['#8B0000', '#FFA500', '#FFFF00', '#228B22', '#40E0D0', '#00BFFF', '#DA70D6', '#8B4513', '#DC143C', '#F0E68C', '#32CD32', '#008080', '#7B68EE', '#9932CC', '#DEB887', '#800000', '#4B0082', '#00BFFF', '#808000', '#FF7F50', '#3CB371' ];
+	const COLORS = ['#8B0000', '#228B22', '#FFA500', '#40E0D0', '#FFFF00', '#DC143C', '#8B4513', '#DA70D6', '#00BFFF', '#F0E68C', '#32CD32', '#008080', '#7B68EE', '#9932CC', '#DEB887', '#800000', '#4B0082', '#00BFFF', '#808000', '#FF7F50', '#3CB371' ];
 
 	
 	return (
@@ -25,12 +34,12 @@ const Stats = (props) => {
 			<p>by learning/mastered subjects</p>
 				<PieChart width={450} height={300}>
 					<Pie
-						data={statsByField}
+						data={sortedStatsByField}
 						innerRadius={60}
 						outerRadius={110}
 						dataKey="value"
 					>
-						{statsByField.map((entry, index) => (
+						{sortedStatsByField.map((entry, index) => (
 							<Cell 
 								key={`cell-${index}`} 
 								fill={COLORS[index % COLORS.length]}
