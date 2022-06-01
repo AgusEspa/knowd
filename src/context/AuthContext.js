a745f8102623799af183f0e88fb689edab8733d5
Agustin Espana <agustinespania@gmail.com>
2022-06-01 17:46:53 -0300

    Update AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -20,8 +20,7 @@ export const AuthProvider = ({ children }) => {
 		navigate("/login");
 	};
 
-	// const baseApiUrl = "https://knowd-api-vuepgx4sjq-uc.a.run.app";
-	const baseApiUrl = "http://localhost:8001";
+	const baseApiUrl = "https://knowd-api-vuepgx4sjq-uc.a.run.app";
 
 	return (
 		<AuthContext.Provider
