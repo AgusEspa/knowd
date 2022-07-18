34d29f07dd4b008b28f35aa8166e4d81f10e5f69
Agustin Espana <agustinespania@gmail.com>
2022-07-18 08:33:31 -0300

    updated texts

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,61 +1,61 @@
 import styles from "../../styles/Home.module.scss";
 
 const About = () => {
-	return (
-		<main className={styles.aboutContainer} id="about">
-			<h2>Welcome to knowd, a relational knowledge tracker</h2>
+    return (
+        <main className={styles.aboutContainer} id="about">
+            <h2>Welcome to knowd, a relational knowledge tracker</h2>
 
-			<p>
-				Built by and for those who regard learning as a lifelong
-				enterprise. <br />
-				Those who know that self guided learning done right has the
-				potential to be one of the most efective and efficient way to
-				gain knowledge and expand our range. <br />
-				Those who cherish being able to adapt a certain curricula to
-				their current state of knowledge and make it flexible enough so
-				that it always feel challenging and rewarding.
-			</p>
+            <p>
+                Built by and for those who regard learning as a lifelong
+                enterprise. <br />
+                Those who know that self guided learning done right has the
+                potential to be one of the most effective and efficient ways to
+                gain knowledge and expand our range. <br />
+                Those who cherish being able to adapt a certain curricula to
+                their current state of knowledge and make it flexible enough so
+                that it always feel challenging and rewarding.
+            </p>
 
-			<h3>Why knowd?</h3>
-			<p>
-				To be certain we are reaping all its benefits and not falling
-				into its traps, it's imperative to track all the steps and make
-				desitions accordingly.
-			</p>
-			<p>
-				You will be able to make entries for any number of subjects and
-				topics, organize them according to the discipline and field of
-				studies.
-			</p>
-			<p>
-				Furthermore, you will be able to connect them thus enriching the
-				potential for further developing your expertise profile and make
-				discoveries beyond what deep specialisation could allow.
-			</p>
+            <h3>Why knowd?</h3>
+            <p>
+                To be certain we are reaping all its benefits and not falling
+                into its traps, it is imperative to track all the steps and make
+                decisions accordingly.
+            </p>
+            <p>
+                You will be able to make entries for any number of subjects and
+                topics, organize them according to the discipline and field of
+                studies.
+            </p>
+            <p>
+                Furthermore, you will be able to connect them thus enriching the
+                potential for further developing your expertise profile and make
+                discoveries beyond what deep specialisation could allow.
+            </p>
 
-			<h3 className={styles.imagesCaptions}>
-				Manage your learning subjects from a simple yet comprehensive
-				dashboard
-			</h3>
-			<div className={styles.bigImageContainer}>
-				<img src="screen1.png" alt="screen sample" />
-			</div>
+            <h3 className={styles.imagesCaptions}>
+                Manage your learning subjects from a simple yet comprehensive
+                dashboard
+            </h3>
+            <div className={styles.bigImageContainer}>
+                <img src="screen1.png" alt="screen sample" />
+            </div>
 
-			<h3 className={styles.imagesCaptions}>
-				Review your learning profile and statistics
-			</h3>
-			<p>
-				Access a comprehensive array of stats, from an overview with all
-				your fields of studies, to more detailed charts of your most
-				complex sets of areas.
-			</p>
-			<div className={styles.smallImagesContainer}>
-				<img src="screen2.png" alt="screen sample" />
-				<img src="screen3.png" alt="screen sample" />
-				<img src="screen4.png" alt="screen sample" />
-			</div>
-		</main>
-	);
+            <h3 className={styles.imagesCaptions}>
+                Review your learning profile and statistics
+            </h3>
+            <p>
+                Access a comprehensive array of stats, from an overview with all
+                your fields of studies, to more detailed charts of your most
+                complex sets of areas.
+            </p>
+            <div className={styles.smallImagesContainer}>
+                <img src="screen2.png" alt="screen sample" />
+                <img src="screen3.png" alt="screen sample" />
+                <img src="screen4.png" alt="screen sample" />
+            </div>
+        </main>
+    );
 };
 
 export default About;
--- src/components/Home/News.js
+++ src/components/Home/News.js
@@ -1,51 +1,51 @@
 import styles from "../../styles/Home.module.scss";
 
 const News = () => {
-	return (
-		<main className={styles.newsContainer} id="news">
-			<p>
-				<span>2 may, 2022</span>
-			</p>
+    return (
+        <main className={styles.newsContainer} id="news">
+            <p>
+                <span>2 july, 2022</span>
+            </p>
 
-			<h2>Mobile Apps</h2>
+            <h2>Mobile Apps</h2>
 
-			<p>
-				To give users the most comprehensive experience posible, I have
-				concentrated my efforts in providing a full desktop web app.{" "}
-				<br />
-				But I also aknowledge the usefullness of having our records on
-				the go. So I'm exited to announce that native Apps for Android
-				and iPhone are on the works and should be coming in the next
-				months.
-			</p>
-			<p>
-				Thank you for all the support and patience throw this
-				developmental period.
-			</p>
-			<p>Agustin.</p>
+            <p>
+                To give users the most comprehensive experience possible, I have
+                concentrated my efforts in providing a full desktop web app.{" "}
+                <br />
+                But I also acknowledge the usefulness of having our records on
+                the go. So I'm exited to announce that native Apps for Android
+                and iPhone are on the works and should be coming in the next
+                months.
+            </p>
+            <p>
+                Thank you for all the support and patience throw this
+                developmental period.
+            </p>
+            <p>Agustin.</p>
 
-			<hr className={styles.newsDivider} />
+            <hr className={styles.newsDivider} />
 
-			<p>
-				<span>14 march, 2022</span>
-			</p>
+            <p>
+                <span>14 may, 2022</span>
+            </p>
 
-			<h2>Beta Version</h2>
+            <h2>Beta Version</h2>
 
-			<p>
-				Knowd is now out of Alpha. While users might experience some
-				slow starts, overall experience should be fast and stable.{" "}
-				<br />
-				The database implementation is now secured and backed up.
-			</p>
-			<p>
-				Upcoming features include:
-				<br />
-				- a better visualization of the relations between subjects, and
-				<br />- more detailed stats charts.
-			</p>
-		</main>
-	);
+            <p>
+                Knowd is now out of Alpha! While users might experience some
+                slow starts, overall experience should be fast and stable.{" "}
+                <br />
+                The database implementation is now secured and backed up.
+            </p>
+            <p>
+                Upcoming features include:
+                <br />
+                - a better visualization of the relations between subjects, and
+                <br />- more detailed stats charts.
+            </p>
+        </main>
+    );
 };
 
 export default News;
